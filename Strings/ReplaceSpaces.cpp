05044ed58d505fa2ff1d45b7a8d8ba3e315ef3bb
Aritra Chatterjee <2004010@kiit.ac.in>
2024-03-07 17:59:23 +0530

    Strings

---
+++ Strings/ReplaceSpaces.cpp
@@ -0,0 +1,23 @@
+// Replace Spaces in a given string
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string replaceSpaces(string &str){
+	string temp = "";
+    
+    for(int i=0; i<str.length(); i++) {
+
+        if(str[i] == ' ') {
+            
+            temp.push_back('@');
+            temp.push_back('4');
+            temp.push_back('0');
+        }
+
+        else temp.push_back(str[i]);
+    }
+
+    return temp;
+}
